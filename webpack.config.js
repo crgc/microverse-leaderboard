db9e93652a8cd61028c1a429706660473bcc0fc8
Carlos Gonzalez <carlosfwrd@gmail.com>
2021-07-28 23:49:37 -0400

    Add dev server

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,12 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+  },
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
